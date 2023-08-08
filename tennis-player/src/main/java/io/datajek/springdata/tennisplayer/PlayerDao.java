ff3ed90aa38d473f9abfbdf293f3883f5494fad5
Yiwei Wu <yiwei.wu@fdmgroup.com>
2023-08-08 21:57:20 +1000

    add update, delete, create table functions in playerDao

--- tennis-player/src/main/java/io/datajek/springdata/tennisplayer/PlayerDao.java
+++ tennis-player/src/main/java/io/datajek/springdata/tennisplayer/PlayerDao.java
@@ -26,13 +26,39 @@ public class PlayerDao {
         String sql = "SELECT * FROM PLAYER WHERE ID = ?";
         return jdbcTemplate.queryForObject(sql,
                 new BeanPropertyRowMapper<Player>(Player.class),
-                new Object[] {id});
+                new Object[]{id});
     }
 
-    public int insertPlayer(Player player){
+    public int insertPlayer(Player player) {
         String sql = "INSERT INTO PLAYER (ID, Name, Nationality, Birth_date, Titles) VALUES (?, ?, ?, ?, ?)";
         return jdbcTemplate.update(sql,
-                new Object[] {player.getId(), player.getName(), player.getNationality(),
+                new Object[]{player.getId(), player.getName(), player.getNationality(),
                         new Timestamp(player.getBirthDate().getTime()), player.getTitles()});
     }
+
+    public int updatePlayer(Player player) {
+        String sql = "UPDATE PLAYER " +
+                "SET Name = ?, Nationality = ?, Birth_date = ? , Titles = ? " +
+                "WHERE ID = ?";
+
+        return jdbcTemplate.update(sql, new Object[]{
+                player.getName(),
+                player.getNationality(),
+                new Timestamp(player.getBirthDate().getTime()),
+                player.getTitles(),
+                player.getId()}
+        );
+    }
+
+    public int deletePlayerById(int id) {
+        String sql = "DELETE FROM PLAYER WHERE ID = ?";
+        return jdbcTemplate.update(sql, new Object[]{id});
+    }
+
+    public void createTournamentTable() {
+        String sql = "CREATE TABLE TOURNAMENT (ID INTEGER, NAME VARCHAR(50), LOCATION VARCHAR(50), PRIMARY KEY (ID))";
+        jdbcTemplate.execute(sql);
+        System.out.println("Table created");
+    }
+
 }
--- tennis-player/src/main/java/io/datajek/springdata/tennisplayer/TennisPlayerApplication.java
+++ tennis-player/src/main/java/io/datajek/springdata/tennisplayer/TennisPlayerApplication.java
@@ -25,7 +25,25 @@ public class TennisPlayerApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 //        logger.info("All Players Data: {}", dao.getAllPlayers());
 //        logger.info("Player with Id 3: {}", dao.getPlayerById(3));
+//        logger.info("All Players Data: {}", dao.getAllPlayers());
+        // Inserting a player
         logger.info("Inserting Player 4: {}", dao.insertPlayer(new Player(4, "Thiem", "Austria", new Date(System.currentTimeMillis()), 17)));
+
+        //Updating a player with bew birthdate
+        logger.info("Updating Player with Id 4: {}", dao.updatePlayer(
+                new Player(4, "Thiem", "Austria",
+                        Date.valueOf("1993-09-03"), 17)));
+
+        //View player by Id
+        logger.info("Players with Id 4: {}", dao.getPlayerById(4));
+
+        // view all players
         logger.info("All Players Data: {}", dao.getAllPlayers());
+        // delete id 2 player
+        logger.info("Deleting Player with Id 2: {}", dao.deletePlayerById(2));
+        // view all players again
+        logger.info("All Players Data: {}", dao.getAllPlayers());
+
+        dao.createTournamentTable();
     }
 }
