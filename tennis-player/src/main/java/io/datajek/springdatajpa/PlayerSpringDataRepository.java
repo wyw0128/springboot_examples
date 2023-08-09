3ebc37d4ee756c4f9d6f507caa0f02642d71805d
Yiwei Wu <yiwei.wu@fdmgroup.com>
2023-08-09 12:34:30 +1000

    updated to Spring Data JPA: add JpaRepository layer

--- tennis-player/src/main/java/io/datajek/springdatajpa/PlayerSpringDataRepository.java
+++ tennis-player/src/main/java/io/datajek/springdatajpa/PlayerSpringDataRepository.java
@@ -1,2 +1,11 @@
-package io.datajek.springdatajpa;public interface PlayerSpringDataRepository {
+package io.datajek.springdatajpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface PlayerSpringDataRepository extends JpaRepository<Player, Integer> {
+    // NOTE: 
+    List<Player> findByNationality(String nationality);
 }
--- tennis-player/src/main/java/io/datajek/springdatajpa/TennisPlayerApplication.java
+++ tennis-player/src/main/java/io/datajek/springdatajpa/TennisPlayerApplication.java
@@ -15,7 +15,8 @@ public class TennisPlayerApplication implements CommandLineRunner {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    PlayerRepository repo;
+    PlayerSpringDataRepository repo;
+
 
     public static void main(String[] args) {
         SpringApplication.run(TennisPlayerApplication.class, args);
@@ -24,22 +25,15 @@ public class TennisPlayerApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        //insert players
-        logger.info("\n\n>> Inserting Player: {}\n", repo.insertPlayer(
-                new Player("Djokovic", "Serbia", Date.valueOf("1987-05-22"), 81)));
-        logger.info("\n\n>> Inserting Player: {}\n", repo.insertPlayer(
-                new Player("Monfils", "France", Date.valueOf("1986-09-01"), 10)));
-        logger.info("\n\n>> Inserting Player: {}\n", repo.insertPlayer(
-                new Player("Thiem", "Austria",
-                        new Date(System.currentTimeMillis()), 17)));
-        //update player
-        logger.info("\n\n>> Updating Player with Id 3: {}\n", repo.updatePlayer(
-                new Player(3, "Thiem", "Austria", Date.valueOf("1993-09-03"), 17)));
-        //get player
-        logger.info("\n\n>> Player with id 3: {}\n", repo.getPlayerById(3));
-        //delete player
-        repo.deletePlayerById(2);
-        //get all players using named query
-        logger.info("All Players Data: {}", repo.getAllPlayers());
+        logger.info("\n\n>>Inserting Player: {}\n", repo.save(new Player("Djokovic", "Serbia", Date.valueOf("1987-05-22"), 81)));
+        logger.info("\n\n>>Inserting Player: {}\n", repo.save(new Player("Monfils", "France", Date.valueOf("1986-09-01"), 10)));
+        logger.info("\n\n>>Inserting Player: {}\n", repo.save(new Player("Thiem", "Austria", new Date(System.currentTimeMillis()), 17)));
+        logger.info("\n\n>>Updating Player with Id 3: {}\n", repo.save(new Player(3, "Thiem", "Austria", Date.valueOf("1993-09-03"), 17)));
+        logger.info("\n\n>>Player with Id 2: {}\n", repo.findById(2));
+        repo.deleteById(2);
+        logger.info("\n\n>>All Players Data: {}\n", repo.findAll());
+
+        logger.info("\n\n>>Players from Austria: {}\n", repo.findByNationality("Austria"));
     }
+
 }
